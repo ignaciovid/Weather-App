286c7f260ace71e1a915ab1729dc0f2acd1ea3d8
unknown <nachovidela03@gmail.com>
2021-05-26 17:20:36 -0300

    change protocol https

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
   
   
   const onSearch = (city) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     fetch(url)
       .then((res) => res.json())
       .then((res) => {
